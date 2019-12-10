fdb6ad71d68e1aa22a329e27cb2abf7ca80a970d
imlizardking57 <56850183+imlizardking57@users.noreply.github.com>
2019-12-10 13:36:02 +0100

    Update keymap.c

--- keyboards/jj50/keymaps/jmmj/keymap.c
+++ keyboards/jj50/keymaps/jmmj/keymap.c
@@ -36,7 +36,7 @@ enum {
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    //Tap once for Esc, twice for Caps Lock
+    //Tap once for Alt, twice for Alt Gr
   [TD_LALT]  = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT)
 };
 
@@ -131,17 +131,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	
     [_LOWER] = LAYOUT( \
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    \
-        _______, KC_GRV,  _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_BSLS, _______,    \
+        _______, _______, _______, _______, _______, _______, _______, _______,  _______,   KC_MINS, KC_EQL,  KC_BSLS,    \
+        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,    \
         _______, _______, _______, KC_LBRC, KC_RBRC, S(KC_9),S(KC_0),S(KC_LBRC),S(KC_RBRC),_______,_______,_______,     \
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-       _______, _______, _______, _______, _______, _______, _______, _______, BL_OFF,  BL_DEC,  BL_INC,  BL_ON    \
+        _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, \
+        _______, _______, _______, _______, _______, _______, _______, _______, BL_OFF,  BL_DEC,  BL_INC,  BL_ON    \
     ),
     
 	
     [_FN] = LAYOUT( \
        RESET, _______, _______, _______, _______, _______, _______, _______, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
-       RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, _______, _______, _______, _______, KC_P7, KC_P8 , KC_P9, KC_PPLS, \
+       _______, _______, _______, _______, _______, _______, _______, _______, KC_P7, KC_P8 , KC_P9, KC_PPLS, \
        _______, _______, _______, _______, _______, _______, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, \
        _______, _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3,KC_PENT, \
        KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, _______, MYRGB_TG    \
