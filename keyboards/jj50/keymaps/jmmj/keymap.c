17e99cbbda92b7fbc299941f5179eb82cf6e3d9a
imlizardking57 <56850183+imlizardking57@users.noreply.github.com>
2020-01-03 19:37:26 +0100

    Update keymap.c
    Change layout to accomodate Spanish users

--- keyboards/jj50/keymaps/jmmj/keymap.c
+++ keyboards/jj50/keymaps/jmmj/keymap.c
@@ -40,7 +40,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LALT]  = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT)
 };
 
-
 enum custom_keycodes {
   MYRGB_TG = SAFE_RANGE
 };
@@ -113,29 +112,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_DEFLT] = LAYOUT( \
-        KC_GESC,	 KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,       \
+        KC_GESC,	 KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_BSPC,       \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,      \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT),		\
-        KC_LCTL, KC_LGUI, TD(TD_LALT) , MO(_FN), MO(_LOWER),KC_SPC,KC_SPC,MO(_RAISE),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT       \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP, SFT_T(KC_ENT),		\
+        KC_LCTL, KC_LGUI, TD(TD_LALT) , MO(_FN), MO(_LOWER),KC_SPC,KC_SPC,MO(_RAISE),KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT       \
     ),
 
 	
     [_RAISE] = LAYOUT( \
-       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-       _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_INSERT, KC_PGUP, KC_PSCR, KC_BRIU,  \
-       _______, _______, _______, _______, _______, _______, _______, KC_END , KC_DELETE, KC_PGDOWN, KC_PAUSE, KC_BRID,  \
-       _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP,  \
-       _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY  \
+       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_MINUS, KC_EQUAL , _______ ,  \
+       KC_F10, KC_F11, KC_F12, _______, _______, _______, _______, _______ , _______  ,KC_LBRACKET ,KC_RBRACKET , KC_BSLASH,  \
+       _______, _______, _______, _______, _______, _______, _______, _______ , _______ , _______ , _______ ,KC_BRIU ,  \
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPLY, KC_BRID,  \
+       _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD, KC_MEDIA_STOP, KC_VOLU\
     ),
 
 	
     [_LOWER] = LAYOUT( \
-        _______, _______, _______, _______, _______, _______, _______, _______,  _______,   KC_MINS, KC_EQL,  KC_BSLS,    \
-        _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,    \
-        _______, _______, _______, KC_LBRC, KC_RBRC, S(KC_9),S(KC_0),S(KC_LBRC),S(KC_RBRC),_______,_______,_______,     \
-        _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG, \
-        _______, _______, _______, _______, _______, _______, _______, _______, BL_OFF,  BL_DEC,  BL_INC,  BL_ON    \
+       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12,  \
+       RGB_HUI, RGB_SAI, RGB_VAI, RGB_TOG , _______ , _______, _______, _______, _______, _______, KC_PSCREEN,  _______,    \
+        _______, _______,  _______, _______, _______, _______, _______, _______,KC_HOME,KC_PGUP, _______,  _______,    \
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_PGDOWN, BL_ON , RGB_TOG, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, BL_DEC, BL_OFF ,  BL_INC\
     ),
     
 	
@@ -144,6 +143,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, _______, _______, _______, _______, _______, _______, _______, KC_P7, KC_P8 , KC_P9, KC_PPLS, \
        _______, _______, _______, _______, _______, _______, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, \
        _______, _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3,KC_PENT, \
-       KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, _______, MYRGB_TG    \
+       _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, KC_PDOT, _______, KC_PENT\
     )
 };
+
+
+
